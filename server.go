d0643f9adf287ac2eda6722680bce5264796b5cb
Dhairya Khanna <dk9582671597@gmail.com>
2024-07-27 17:49:00 +0530

    feat: Added Routing using URL

--- server.go
+++ server.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w,"20");
+		player:= strings.TrimPrefix(r.URL.Path,"/players/")
+		if player == "Pepper"{
+			fmt.Fprint(w,"20")
+		}
+		if player == "Floyd"{
+			fmt.Fprint(w,"10")
+		}
 }
 func main(){
 	handler := http.HandlerFunc(PlayerServer)
