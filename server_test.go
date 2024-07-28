60c2f56485eb312684c2e15ca22b85ace7125a6c
Dhairya Khanna <dk9582671597@gmail.com>
2024-07-28 23:12:06 +0530

    feat: Added status code for non-existent players

--- server_test.go
+++ server_test.go
@@ -24,16 +24,30 @@ func TestGETPlayers(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
-
 		server.ServeHTTP(response,request)
+		assertStatus(t,response.Code,http.StatusOK)
 		assertResponseBody(t,response.Body.String(),"20")
 	})
 	t.Run("returns Floyd's score", func(t *testing.T) {
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response,request)
+		assertStatus(t,response.Code,http.StatusOK)
 		assertResponseBody(t,response.Body.String(),"10")
 	})
+	t.Run("returns 404 on missing players",func(t *testing.T){
+		request := newGetScoreRequest("Jack")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response,request)
+		assertStatus(t,response.Code,http.StatusNotFound)
+		
+	})
+}
+func assertStatus(t testing.TB,got,want int){
+	t.Helper()
+	if got!=want{
+		t.Errorf("got status %d want %d",got,want)
+	}
 }
 func assertResponseBody(t testing.TB,got,want string){
 	t.Helper()
