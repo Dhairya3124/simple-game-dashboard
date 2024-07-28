34fe2dd38526e8f8eb1e7ae5f286bc0d575a499c
Dhairya Khanna <dk9582671597@gmail.com>
2024-07-28 22:32:39 +0530

    fix: invalid memory address

--- server.go
+++ server.go
@@ -6,10 +6,15 @@ import (
 	"net/http"
 	"strings"
 )
-
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerStore interface{
+	GetPlayerScore(name string)int
+}
+type PlayerServer struct{
+	store PlayerStore
+}
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		player:= strings.TrimPrefix(r.URL.Path,"/players/")
-		fmt.Fprint(w,GetPlayerScore(player))
+		fmt.Fprint(w,p.store.GetPlayerScore(player))
 		
 }
 func GetPlayerScore(name string)string{
@@ -22,7 +27,12 @@ func GetPlayerScore(name string)string{
 	return ""
 
 }
-func main(){
-	handler := http.HandlerFunc(PlayerServer)
-	log.Fatal(http.ListenAndServe(":5000",handler))
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
