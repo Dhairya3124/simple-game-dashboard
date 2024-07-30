86f47b7132de8dd9e52e321a09837523c8c405ea
Dhairya Khanna <dk9582671597@gmail.com>
2024-07-30 23:08:26 +0530

    feat: Added POST Requests and tests

--- server.go
+++ server.go
@@ -12,14 +12,32 @@ type PlayerStore interface{
 type PlayerServer struct{
 	store PlayerStore
 }
+//server.go
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-		player:= strings.TrimPrefix(r.URL.Path,"/players/")
-		score:=p.store.GetPlayerScore(player)
-		if score == 0{
-			w.WriteHeader(http.StatusNotFound)
-		}
-		fmt.Fprint(w,score)
-		
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
 func GetPlayerScore(name string)string{
 	if name == "Pepper"{
