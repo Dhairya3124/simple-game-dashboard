bb82dd8f0cffc1991d913ff43430039ad064c4c1
Dhairya Khanna <dk9582671597@gmail.com>
2024-08-03 14:51:01 +0530

    feat: Added integration test and mutex for concurrency

--- main.go
+++ main.go
@@ -5,15 +5,7 @@ import (
 	"net/http"
 )
 
-// InMemoryPlayerStore collects data about players in memory.
-type InMemoryPlayerStore struct{}
-
-// GetPlayerScore retrieves scores for a given player.
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
