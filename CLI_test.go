d30ba22323fc2b07a97fdbe41fc5d536d7947825
Dhairya Khanna <dk9582671597@gmail.com>
2024-08-22 21:33:17 +0530

    feat: Added RecordWin in cmd

--- CLI_test.go
+++ CLI_test.go
@@ -6,12 +6,25 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
 
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
-	assertPlayerWin(t,playerStore,"Chris")
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
 }
 func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string){
 	t.Helper()
