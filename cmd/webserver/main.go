68982a5a831b5bd000eff8bfaa2aa75dc74317d0
Dhairya Khanna <dk9582671597@gmail.com>
2024-08-24 16:41:01 +0530

    refractor: main of cli and webserver

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-	poker "github.com/Dhairya3124/simple-game-dashboard.git"
 	"log"
 	"net/http"
-	"os"
+
+	poker "github.com/Dhairya3124/simple-game-dashboard.git"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
-	}
+	defer close()
 
 	server := poker.NewPlayerServer(store)
 
-	log.Fatal(http.ListenAndServe(":5000", server))
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
