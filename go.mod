c436397808cb40797b4a7f49d016e8c9eaa514d1
Dhairya Khanna <dk9582671597@gmail.com>
2024-09-01 23:13:32 +0530

    test: added test for Websockets

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Dhairya3124/simple-game-dashboard.git
 
 go 1.22.3
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server_test.go
+++ server_test.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/gorilla/websocket"
 )
 
 func TestGETPlayers(t *testing.T) {
@@ -129,6 +132,26 @@ func TestGame(t *testing.T){
 		response:=httptest.NewRecorder()
 		server.ServeHTTP(response,request)
 		assertStatus(t,response.Code,http.StatusOK)
+	})
+	t.Run("when we get a message over a websocket it is a winner of a game", func(t *testing.T) {
+		store := &StubPlayerStore{}
+		winner := "Ruth"
+		server := httptest.NewServer(NewPlayerServer(store))
+		defer server.Close()
+	
+		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
+	
+		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+		if err != nil {
+			t.Fatalf("could not open a ws connection on %s %v", wsURL, err)
+		}
+		defer ws.Close()
+	
+		if err := ws.WriteMessage(websocket.TextMessage, []byte(winner)); err != nil {
+			t.Fatalf("could not send message over ws connection %v", err)
+		}
+	
+		AssertPlayerWin(t, store, winner)
 	})
 }
 func getLeagueFromResponse(t testing.TB, body io.Reader) []Player {
