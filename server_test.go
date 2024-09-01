0d40b1bac4869c5ee559f2aadbd98d6ab9472bc1
Dhairya Khanna <dk9582671597@gmail.com>
2024-09-01 15:01:17 +0530

    feat: added markup for our site

--- server_test.go
+++ server_test.go
@@ -125,7 +125,7 @@ func TestLeague(t *testing.T) {
 func TestGame(t *testing.T){
 	t.Run("GET /game returns 200",func(t *testing.T) {
 		server:=NewPlayerServer(&StubPlayerStore{})
-		request,_:=http.NewRequest(http.MethodGet,"/game",nil)
+		request := newGameRequest()
 		response:=httptest.NewRecorder()
 		server.ServeHTTP(response,request)
 		assertStatus(t,response.Code,http.StatusOK)
@@ -172,7 +172,10 @@ func newPostWinRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
 	return req
 }
-
+func newGameRequest() *http.Request{
+	req,_ := http.NewRequest(http.MethodGet,"/game",nil)
+	return req
+}
 func assertResponseBody(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
