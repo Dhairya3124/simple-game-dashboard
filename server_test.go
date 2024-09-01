5e97acd1fd557bbe5408d2283cd5a1adb48a936f
Dhairya Khanna <dk9582671597@gmail.com>
2024-09-01 14:38:20 +0530

    test: intialising web sockets

--- server_test.go
+++ server_test.go
@@ -122,6 +122,15 @@ func TestLeague(t *testing.T) {
 
 	})
 }
+func TestGame(t *testing.T){
+	t.Run("GET /game returns 200",func(t *testing.T) {
+		server:=NewPlayerServer(&StubPlayerStore{})
+		request,_:=http.NewRequest(http.MethodGet,"/game",nil)
+		response:=httptest.NewRecorder()
+		server.ServeHTTP(response,request)
+		assertStatus(t,response.Code,http.StatusOK)
+	})
+}
 func getLeagueFromResponse(t testing.TB, body io.Reader) []Player {
 	t.Helper()
 	league, err := NewLeague(body)
